9b0334367a30ddf3684097cd68facc680bef5c21
vgentile0 <gentile.vincenzo.001@gmail.com>
2024-10-22 11:13:25 +0200

    Update IpResolver.py

--- IpResolver.py
+++ IpResolver.py
@@ -1,13 +1,24 @@
 import socket
 
-def ipHandler ():
+def ipHandler():
     target_ip = ""
     ip_type = int(input("Type 1 to scan by domain or 2 to scan by IPv4:  "))
 
-# ADD URL parser to automatically add www.
-    if ip_type == 1 :
-        target_ip = socket.gethostbyname(input("Enter target domain: "))
-    if ip_type == 2 :
+    if ip_type == 1:
+        domain = input("Enter target domain: ")
+
+        #  URL parser - automatically add www. if not present
+        if not domain.startswith("www."):
+            domain = "www." + domain
+
+        try:
+            target_ip = socket.gethostbyname(domain)
+        except socket.gaierror:
+            print("Invalid domain name.")
+            return None
+
+    elif ip_type == 2:
         target_ip = input("Enter target IP: ")
 
     return target_ip
+
